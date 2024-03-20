1f573bdd17be624ba773febdc33e5c45722d74be
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2024-03-20 15:05:50 +0000

    Added new methods in the collections exercises

--- src/main/java/part02/CollectionsBasics.java
+++ src/main/java/part02/CollectionsBasics.java
@@ -34,6 +34,37 @@ public class CollectionsBasics {
     }
 
     /**
+     * Returns a new list, where the numbers from the given list are multiplied by
+     * the given factor. The order of the numbers in the new list should be the same
+     * as in the original list. For example, if the given list contains the
+     * numbers -1, 0 and 100 and the factor is 2, the result is a new list
+     * containing the numbers -2, 0 and 200.
+     *
+     * The original list should not be modified. Instead, a new list should be
+     * created and returned.
+     *
+     * @param numbers A list of integers.
+     * @return A new list, where the numbers are multiplied by the given factor.
+     */
+    public List<Integer> multiply(List<Integer> numbers, int factor) {
+        return null; // TODO: implement this method
+    }
+
+    /**
+     * Generates and returns a list, which contains integer numbers between the
+     * given start and end values. The start value is included in the list, but the
+     * end value is not. The numbers must be in ascending order. If the end value is
+     * less than or equal to the start value, an empty list is returned.
+     *
+     * @param start The start value, included in the list.
+     * @param end   The end value, not included in the list.
+     * @return A list containing the numbers between the start and end values.
+     */
+    public List<Integer> range(int start, int end) {
+        return null; // TODO: implement this method
+    }
+
+    /**
      * Concatenates (or joins) a list of strings into a single string.
      * For example, if the list contains the strings "foo", "bar" and "baz",
      * the result is "foobarbaz". You can assume that the list is not empty.
@@ -54,6 +85,26 @@ public class CollectionsBasics {
      * @return A list containing the lengths of the strings in the input list.
      */
     public List<Integer> getLengths(List<String> strings) {
+        return null; // TODO: implement this method
+    }
+
+    /**
+     * Returns all the indices in the given text where the given substring is found
+     * at. If the substring is not found in the text, an empty list is returned. The
+     * search is case-sensitive and the indices are zero-based, just like in the
+     * indexOf method of the String class.
+     *
+     * The substrings may overlap, in which case the method should return all the
+     * indices where the substring starts. For example, if the text is "banana" and
+     * the substring is "ana", the result is a list containing the numbers 1 and 3.
+     *
+     * @param text      The text to search from.
+     * @param substring The substring to search for.
+     * @return A list containing all the indices where the substring is found at.
+     */
+    public List<Integer> indicesOf(String text, String substring) {
+        // You can assume that the given substring is not empty.
+
         return null; // TODO: implement this method
     }
 }
